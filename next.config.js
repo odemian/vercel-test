84ad12a7ad44cddb001b504cb6b102f444d0652f
odemian <oleksandr@addonsforgapps.com>
2021-05-03 16:07:56 +0200

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -1,27 +1,3 @@
 module.exports = {
-	async rewrites() {
-		return [
-			{
-				source: '/blog/:slug',
-				destination: 'https://yamm.ghost.io/:slug'
-			},
-			{
-				source: '/blog',
-				destination: 'https://yamm.ghost.io'
-			},
-			{
-				source: '/asset/:slug*',
-				destination: 'https://yamm.ghost.io/asset/:slug*'
-			},
-			
-			{
-				source: '/content/:slug*',
-				destination: 'https://yamm.ghost.io/content/:slug*'
-			},
-			{
-				source: '/private',
-				destination: 'https://yamm.ghost.io/private/?r=%2F'
-			},
-		]
-	}
+
 }
